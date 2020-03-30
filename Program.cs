b89081e17cfe50116dec27890c1f8cf104f2ff6e
unknown <lvova.nikol@mail.ru>
2020-03-30 19:21:21 +0300

    created console .net core project

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Dragon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dragons, bulletts;
+            Console.Write("Введите количество драконов: ");
+            dragons = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество пуль: ");
+            bulletts = Convert.ToInt32(Console.ReadLine());
+            bool b = (dragons<=bulletts/2);
+            if(b==true) Console.WriteLine("True");
+            else Console.WriteLine("False");
+        }
+    }
+}
